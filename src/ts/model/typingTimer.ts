16d098b61d9eb114fb5055f92c95731aa99a56e3
Lauritz Bjerre Thomsen <git@lauritzt.dk>
2025-06-15 19:01:21 +0200

    Use KeystrokeHistogram in TypingTimer
    Re #23

--- src/ts/model/typingTimer.ts
+++ src/ts/model/typingTimer.ts
@@ -1,22 +1,29 @@
 import { CorrectLetterInputEvent } from "../events/input/correctLetterInputEvent.js";
 import { Module } from "../module.js";
+import { KeystrokeHistogram } from "./keystrokeHistogram.js";
+import * as persistance from "../controller/persistance.js";
 
 export class TypingTimer implements Module {
+	keystrokeHistogram = new KeystrokeHistogram();
+
 	constructor() {
+
+	}
+
+	recordInput(e: CorrectLetterInputEvent) {
+		if (e.timeTaken !== undefined) {
+			this.keystrokeHistogram.insertKeystroke(e.letter, e.timeTaken);
+		}
+	}
+
+	initialize(): void {
+		// TODO #23 Read from persistant storage
 		CorrectLetterInputEvent.subscribe(
 			(e: CorrectLetterInputEvent) => {this.recordInput(e)}
 		);
 	}
 
-	recordInput(e: CorrectLetterInputEvent) {
-		// TODO #23
-	}
-
-	initialize(): void {
-
-	}
-
 	drop(): void {
-
+		// TODO #23 Save to persistant storage
 	}
 }
